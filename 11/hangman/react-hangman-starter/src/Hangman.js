748e06e6ad3cf6482128732b4884d97fbbe78330
Garrett Thor <garrettthor@gmail.com>
2021-08-17 10:02:57 -0700

    hangman walkthrough complete

--- 11/hangman/react-hangman-starter/src/Hangman.js
+++ 11/hangman/react-hangman-starter/src/Hangman.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {randomWord} from './words.js';
 import "./Hangman.css";
 import img0 from "./0.jpg";
 import img1 from "./1.jpg";
@@ -7,6 +8,7 @@ import img3 from "./3.jpg";
 import img4 from "./4.jpg";
 import img5 from "./5.jpg";
 import img6 from "./6.jpg";
+
 
 class Hangman extends Component {
   /** by default, allow 6 guesses and use provided gallows images. */
@@ -17,8 +19,9 @@ class Hangman extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
     this.handleGuess = this.handleGuess.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   /** guessedWord: show current-state of word:
@@ -46,6 +49,8 @@ class Hangman extends Component {
   generateButtons() {
     return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
       <button
+      // SOLUTION PART ONE - easy to implement a key here bc each letter is only used once, so why not just use ltr..
+        key={ltr}
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
@@ -55,14 +60,43 @@ class Hangman extends Component {
     ));
   }
 
+  reset() {
+      this.setState(st => ({
+        nWrong: 0, 
+        guessed: new Set(), 
+        answer: randomWord() 
+    })); 
+  }
+
   /** render: render game */
   render() {
+    const gameOver = this.state.nWrong >= this.props.maxWrong;
+    const altText = `${this.state.nWrong}/${this.props.maxWrong} guesses`;
+    let winner = this.guessedWord().join("") === this.state.answer;
+    let gameState = this.generateButtons();
+    if(winner) gameState = "You Win!";
+    if (gameOver) gameState = "You Lose!";
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <img src={this.props.images[this.state.nWrong]} alt={altText} />
+
+        {/* SOLUTION PART TWO: adding the guessed wrong section is simple, because it's already being tracked in this.state.nWrong */}
+        <p>Guessed Wrong: {this.state.nWrong}</p>
+
+        <p className='Hangman-word'>
+            {!gameOver
+                ? this.guessedWord()
+                : this.state.answer}</p>
+
+        <p className='Hangman-btns'>
+            {gameState}
+        </p>
+
+
+
+        <button id="Hangman-Reset-Button" onClick={this.reset}>Restart?</button>
+        
       </div>
     );
   }
