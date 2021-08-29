8cc246301c7e4982a55819ea0d7e5587b0a5d7d2
Garrett Thor <garrettthor@gmail.com>
2021-08-29 09:18:29 -0700

    moved handleClick function up under the constructor instead of inline arrow

--- 16/yahtzee-starter/src/Die.js
+++ 16/yahtzee-starter/src/Die.js
@@ -2,13 +2,22 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(){
+        this.props.handleClick(this.props.idx)
+    }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
         // add an inline arrow function, and then pass this.props.idx as the argument to be passed up, eventually to toggleLocked in Game.js
-        onClick={() => this.props.handleClick(this.props.idx)}
+        // we're going to move the function up to under the Component in order to avoid the inline arrow func
+        // onClick={() => this.props.handleClick(this.props.idx)}
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
