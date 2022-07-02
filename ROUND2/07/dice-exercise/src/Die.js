9cedaafe460e14b00a2098daff05311e82819c57
Garrett Thor <garrettthor@gmail.com>
2022-07-02 09:55:15 -0700

    Finish 07, Dice Exercise app, styling and animation

--- ROUND2/07/dice-exercise/src/Die.js
+++ ROUND2/07/dice-exercise/src/Die.js
@@ -6,7 +6,7 @@ class Die extends Component {
     render(){
         return(
             <div>
-                <i className={`fas fa-dice-${this.props.face}`}></i>
+                <i className={`fas fa-dice-${this.props.face} ${this.props.rolling ? 'shaking' : ''}`}></i>
             </div>
         )
     }
--- ROUND2/07/dice-exercise/src/RollDice.js
+++ ROUND2/07/dice-exercise/src/RollDice.js
@@ -12,28 +12,42 @@ class RollDice extends Component{
         super(props);
         this.state = {
             die1: 'one',
-            die2: 'two'
+            die2: 'two',
+            rolling: false
         }
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(e) {
-        let rand1 = Math.floor(Math.random() * 6);
-        this.setState({ die1: this.props.sides[rand1] });
-        let rand2 = Math.floor(Math.random() * 6);
-        this.setState({ die2: this.props.sides[rand2]});
+        if(this.state.rolling === false){
+            let rand1 = Math.floor(Math.random() * 6);
+            this.setState({ die1: this.props.sides[rand1] });
+
+            let rand2 = Math.floor(Math.random() * 6);
+            this.setState({ die2: this.props.sides[rand2]});
+
+            this.setState({ rolling: true });
+            setTimeout(() => {
+                this.setState({ rolling: false });
+            }, 1000);
+        } else {
+            return;
+        };
+        
     }
 
     render() {
         return(
             <div className="RollDice">
                 <section className='RollDice-Die-Container'>
-                    <Die face={this.state.die1}/>
-                    <Die face={this.state.die2}/>
+                    <Die face={this.state.die1} rolling={this.state.rolling}/>
+                    <Die face={this.state.die2} rolling={this.state.rolling}/>
                 </section>
 
                 <section className='RollDice-Button-Container'>
-                    <button onClick={this.handleClick}>Roll the Dice!</button>
+                    <button onClick={this.handleClick}>
+                        {this.state.rolling ? 'Rolling...' : 'Roll Dice!'}
+                    </button>
                 </section>
             </div>
         )
